5d98e99c0dfd283f841ac52177b30d6e0e1044ea
Jesus Garcia <jesusgarcia@Kubo-MacBook-2.local>
2022-09-25 10:52:15 -0500

    navbar design was finished

--- src/components/organisms/navbar/Navbar.js
+++ src/components/organisms/navbar/Navbar.js
@@ -1,41 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 
 import Logo from "img/logo-simple.svg";
 import Menu from "@/assets/img/menu.svg";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState("close");
+
+  const clickHamburgerMenu = () => {
+    setOpenMenu(openMenu === "open" ? "close" : "open");
+    document.body.style.overflow = openMenu === "open" ? "" : "hidden";
+  };
   return (
-    <div className="navbar-page">
-      <div className="vertical-center">
-        <div className="grid">
-          <div className="link-container">
-            <div>
-              <a href="#nosotros">NOSOTROS</a>
-              <a href="#productos">PRODUCTOS</a>
+    <>
+      <div className="navbar-page">
+        <div className="vertical-center">
+          <div className="grid">
+            <div className="link-container">
+              <div>
+                <a href="#nosotros">NOSOTROS</a>
+                <a href="#productos">PRODUCTOS</a>
+              </div>
+              <div className="banderitas-container">
+                <div></div>
+              </div>
             </div>
-            <div className="banderitas-container">
-              <div></div>
+            <div className="logo-container">
+              <Logo />
             </div>
-          </div>
-          <div className="logo-container">
-            <Logo />
-          </div>
-          <div className="link-container">
-            <div>
-              <a href="#carta">CARTA</a>
-              <a href="#horario">HORARIO</a>
+            <div className="link-container">
+              <div>
+                <a href="#carta">CARTA</a>
+                <a href="#horario">HORARIO</a>
+              </div>
+              <div className="banderitas-container right">
+                <div></div>
+              </div>
             </div>
-            <div className="banderitas-container right">
-              <div></div>
+            <div
+              className="menu-container vertical-center"
+              onClick={clickHamburgerMenu}
+            >
+              <Menu />
             </div>
-          </div>
-          <div className="menu-container vertical-center">
-            <Menu />
           </div>
         </div>
       </div>
-    </div>
+      <nav className={`container mobile-menu ${openMenu}`}>
+        <div className="background"></div>
+        <div className="row">
+          <div>
+            <a href="#nosotros">NOSOTROS</a>
+          </div>
+          <div>
+            <a href="#productos">PRODUCTOS</a>
+          </div>
+          <div>
+            <a href="#carta">CARTA</a>
+          </div>
+          <div>
+            <a href="#horario">HORARIO</a>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 export default Navbar;
